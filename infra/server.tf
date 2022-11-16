24008fe8370ce0efdbedf82f2eccde838cd9750f
RedlineTriad <39059512+RedlineTriad@users.noreply.github.com>
2022-11-16 18:43:33 +0100

    Store docker data on seperate volume

--- infra/server.tf
+++ infra/server.tf
@@ -2,11 +2,20 @@ resource "hcloud_server" "web" {
   name        = "web"
   image       = "debian-11"
   server_type = "cpx11"
+  location    = "nbg1"
   ssh_keys    = [hcloud_ssh_key.ansible.name]
   public_net {
     ipv4_enabled = true
     ipv6_enabled = true
   }
+}
+
+resource "hcloud_volume" "data" {
+  name      = "data"
+  size      = 10
+  server_id = hcloud_server.web.id
+  automount = true
+  format    = "ext4"
 }
 
 resource "hcloud_ssh_key" "ansible" {
