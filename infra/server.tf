7ebb2f115b0e58f4f588201502df0f5022837292
RedlineTriad <39059512+RedlineTriad@users.noreply.github.com>
2022-12-23 19:56:39 +0100

    Extract domain names for docker-compose files

--- infra/server.tf
+++ infra/server.tf
@@ -89,8 +89,16 @@ resource "cloudflare_record" "aaaa" {
   ttl     = 300
 }
 
+locals {
+  docker_compose_file_paths = fileset(".", "../software/roles/docker_compose_apps/templates/*/docker-compose.yml")
+  docker_compose_data       = [for path in local.docker_compose_file_paths : yamldecode(file(path))]
+  services                  = flatten([for data in local.docker_compose_data[*].services : values(data)])
+  labels                    = flatten([for service in local.services : try(service.labels, [])])
+  subdomains                = flatten([for label in local.labels : try(regex(".*Host\\(`(\\w+)\\.\\$BASE_DOMAIN`\\).*", label), [])])
+}
+
 resource "cloudflare_record" "aliases" {
-  for_each = toset(["grocy"])
+  for_each = toset(local.subdomains)
   zone_id  = cloudflare_zone.personal_domain.id
   name     = each.key
   value    = "@"
