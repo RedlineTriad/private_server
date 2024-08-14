a6c95d73da6749d975f070fb4b02ef64cc4a6d58
RedlineTriad <39059512+RedlineTriad@users.noreply.github.com>
2024-08-14 23:01:53 +0200

    feat: downscale for reduced costs

--- infra/server.tf
+++ infra/server.tf
@@ -1,7 +1,7 @@
 resource "hcloud_server" "web" {
   name        = "web"
   image       = "debian-11"
-  server_type = "cpx21"
+  server_type = "cx22"
   location    = "nbg1"
   ssh_keys    = [hcloud_ssh_key.ansible.name]
   public_net {
